6d856adffa7c82ef00b1a5edecc021ba4d283770
lylee76 <pakhrinamylee@gmail.com>
2026-04-06 08:31:39 +0545

    added day17.py file

---
+++ Github/day2.py
@@ -0,0 +1,12 @@
+# git branching concept
+
+# to view all the branches
+# git branch
+
+# to create a new branch
+# git branch {branch_name} # only creates
+# git chechout -b {branch_name} # creates and also switches 
+
+# to switch between branches
+# git switch {branch_name}
+# git checkout {branch_name}
